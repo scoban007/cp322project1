e57bc17b8a598a890318b082e83bf8743ce8eb16
neclatefon <128425936+neclatefon@users.noreply.github.com>
2023-10-10 21:14:54 -0400

    Add files via upload
    it's not working, i cant split/test data for the life of me :D but this is just the logic....at least ithink.................

---
+++ KNN.py
@@ -0,0 +1,147 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from ucimlrepo import fetch_ucirepo 
+import math
+import operator
+import random
+from collections import Counter
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+ionosphere = fetch_ucirepo(id=52) 
+X = ionosphere.data.features 
+y = ionosphere.data.targets
+
+
+class KNN:
+
+    def __init__(self, K=3):
+        self.K = K          #define k, which is 3 for now
+        self.xArray = np.array(X)
+        self.yArray = np.array(y)
+        return
+    
+    def fit(self, points):
+        self.points = points #we dont need much for fit
+
+    def euclidean(self, p , q):
+         return np.sqrt(np.sum(np.array(p) - np.array(q)) ** 2) #euclidean for finding distance between every point in p and q.
+    
+    def train_test_split(X, y, train_size, shuffle=False):
+        length_dataset = len(X)
+        length_train = int(length_dataset * train_size)
+
+        # Shuffle dataset x and y in the same way
+        if shuffle:
+            combine = np.arange(X.shape[0])
+            np.random.shuffle(combine)
+            X = X[combine]
+            y = y[combine]
+
+        # Split as training and test
+        X_train = X[:length_train, :]
+        X_test = X[length_train:, :]
+        y_train = y[:length_train]
+        y_test = y[length_train:]
+
+        return X_train, X_test, y_train, y_test
+    
+    def knn(X_train, y_train, X_test, k):
+        temp_prediction = []
+        temp_distances = []
+        distances = []
+        prediction = []
+        common = []
+
+        for i in range(len(X_train)):
+            for j in range(len(X_test)):
+                temp_distances.append([KNN.euclidean(X_train[i], X_test[j]), j])
+            temp_distances.sort()
+            distances = temp_distances[0:KNN.k]
+            for x, y in distances:
+                temp_prediction.append(y_train[y])
+            prediction.append(Counter(temp_prediction).most_common(1)[0][0])
+        return prediction
+            
+            
+        
+
+
+
+
+
+
+
+
+
+split_ratio = 0.8
+X_train, y_train, X_test, y_test = KNN.train_test_split(X, y, split_ratio)
+
+# Set the value of K (you can change this to experiment)
+k = 3
+
+# Make predictions using the KNN algorithm
+y_pred = KNN.knn(X_train, y_train, X_test, k)
+
+# Calculate and print the accuracy
+correct_predictions = sum(1 for true, pred in zip(y_test, y_pred) if true == pred)
+accuracy = correct_predictions / len(y_test)
+print(f"Accuracy of KNN with k={k}: {accuracy:.2f}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
