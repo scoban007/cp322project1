1c1c11007ff115044ee1ccd71dbfc88f8fb2e39f
neclatefon <128425936+neclatefon@users.noreply.github.com>
2023-10-13 00:14:30 -0400

    Add files via upload

---
+++ TestDataset1KNN.py
@@ -0,0 +1,28 @@
+from ucimlrepo import fetch_ucirepo
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+from KNN import KNN
+
+ionosphere = fetch_ucirepo(id=52) 
+x = ionosphere.data.features 
+y = ionosphere.data.targets 
+
+xArray = np.array(x)
+yArray = np.array(y)
+yList = []
+xTest = []
+
+xTest = xArray[0:5]
+
+for j in yArray:
+    if j == "g":
+        yList.append(1)
+    else:
+        yList.append(0)
+
+sd = KNN.predicts(xArray, yList, xTest, 5)
+#print(sd)
+temp_sd = KNN.temp_predicts(xArray, yList, xTest, 5)
+print(temp_sd)
+print(KNN.evaluate_acc(temp_sd, yList[:5]))
---
+++ TestDataset2KNN.py
@@ -0,0 +1,40 @@
+from ucimlrepo import fetch_ucirepo
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+from KNN import KNN
+
+adult = fetch_ucirepo(id=2) 
+x = adult.data.features 
+y = adult.data.targets 
+
+xArray = np.array(x)
+yArray = np.array(y)
+xFeature = []
+yList = []
+xTest = []
+
+
+for i in range(250):
+    xFeature.append(xArray[i][10])
+    xFeature.append(xArray[i][11])
+    xFeature.append(xArray[i][12])
+
+for i in range(5):
+    xTest.append(xArray[i][10])
+    xTest.append(xArray[i][11])
+    xTest.append(xArray[i][12])
+
+print(xTest)
+
+for j in range(250):
+    if yArray[j] == ">50K":
+        yList.append(0)
+    else:
+        yList.append(1)
+
+sd = KNN.predicts(xFeature, yList, xTest, 5)
+#print(sd)
+temp_sd = KNN.temp_predicts(xFeature, yList, xTest, 5)
+print(temp_sd)
+print(KNN.evaluate_acc(temp_sd, yList[:5]))
