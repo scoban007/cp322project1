719899cec47d6d109e5901e30740a8d8df33d458
neclatefon <128425936+neclatefon@users.noreply.github.com>
2023-10-13 22:07:31 -0400

    Update TestDataset1KNN.py

--- TestDataset1KNN.py
+++ TestDataset1KNN.py
@@ -13,16 +13,19 @@ yArray = np.array(y)
 yList = []
 xTest = []
 
-xTest = xArray[0:5] #define k as 5
 
-for j in yArray:
+xTest = xArray[0:20] #defining xTest 
+#  xArray = xArray[5:10] #defining xArray, making it so that it contains different values than from xTest
+
+for j in yArray: #making yList an int based list
     if j == "g":
         yList.append(1)
     else:
         yList.append(0)
 
-sd = KNN.predicts(xArray, yList, xTest, 5) 
-#print(sd)
-temp_sd = KNN.temp_predicts(xArray, yList, xTest, 5) #need all of the predictions until k to compare and get accuracy
+sd = KNN.predicts(xArray, yList, xTest, 3) #pick k.
+print(sd)
+temp_sd = KNN.temp_predicts(xArray, yList, xTest, 3) #needed for evaluating accuracy, this list contains the targets of K  closest neighbours to x
 #print(temp_sd)
-#print(KNN.evaluate_acc(temp_sd, yList[:5]))
+print(KNN.evaluate_acc(temp_sd, yList[0:3])) #get accuracy
+#print("\nAverage accuracy is {:.2f} % with k-fold".format(KNN.kfold(xArray, yList, 21, 3))) #not working...
